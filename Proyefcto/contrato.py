7d90d9c929d9607e251c9c090f7f3a6d0850fd17
Abel <abe.l240806@gmail.com>
2024-11-09 13:15:18 -0300

    gitt commit -m "Contrato (me está matando)"

--- Proyefcto/contrato.py
+++ Proyefcto/contrato.py
@@ -1,4 +1,5 @@
 import datetime
+import inspect
 import empleado
 class Contrato:
     # inicializa los legajos en 0
@@ -25,29 +26,45 @@ class Contrato:
             print("Formato de fecha incorrecto(Debe ingresar Día/Mes/año. Ejemplo: 24/08/2006)")
             self.fecha_contrato()
 
-    # función recursiva para verificar que un input no esté vacío
-    def no_input(self, x):
+
+    def puesto(self):
         while True:
-            if x.strip() == "":
-                print("La entrada no puede estar vacía")
-                self.no_input()
+            self.puesto = input("Ingrese el puesto del trabajador: ")
+            if self.puesto == "":
+                print("El puesto no puede ser vacio")
             else:
                 break
-        
-            
-            
+
+
+    def sueldo(self):
+        sueld_min = 2798309
+        while True:
+            try:
+                self.sueldo = int(input("Ingrese el sueldo del trabajador: "))
+                if self.sueldo < sueld_min:
+                    print("ERROR: El sueldo no puede ser menor al sueldo minimo.")
+                else:
+                    break
+            except ValueError:
+                print("ERROR: Debe ingresar solo el monto del sueldo")
+    
     
     def registro(self):
+        # Asignar la fecha de firma del contrato
         self.fecha_contrato()
         
         # Asigna un número de legajo y automáticamente suma +1
         self.idlegajo = Contrato.legajo
         Contrato.legajo += 1
         
-        self.puesto = input("Ingrese el puesto del trabajador: ")
-        self.no_input(self.puesto)
+        # Define un puesto para el contrato
+        self.puesto()
         
-        self.
+        # Define un sueldo
+        self.sueldo()
+        
+        
+        
 
         
         
--- Proyefcto/example.py
+++ Proyefcto/example.py
@@ -6,3 +6,18 @@ formato = "%Y-%m-%d"      # formato de la cadena de fecha
 
 fecha_objeto = datetime.strptime(fecha_str, formato)
 print(fecha_objeto)
+
+
+
+
+ # función recursiva para verificar que un input no esté vacío
+    def no_input(self, valor, mensaje):
+        if not valor.strip():
+            print("El valor no puede estar vacío. Inténtelo de nuevo.")
+            # Obtener el nombre del atributo llamador
+            frame = inspect.currentframe().f_back
+            atributo = [name for name, val in frame.f_locals.items() if val is valor][0]
+            # Solicitar nuevamente el input y llamar recursivamente
+            nuevo_valor = input(mensaje)
+            return self.no_input(nuevo_valor, mensaje)
+        return valor
